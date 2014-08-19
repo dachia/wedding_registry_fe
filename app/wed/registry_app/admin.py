f3074e32205d7d0863d5fae2cd279d696b4cefa6
Aleksej Klebanskij <mail@willdo.lt>
2014-08-19 14:55:48 +0300

    initial form changes

--- app/wed/registry_app/admin.py
+++ app/wed/registry_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from registry_app.models import Item, Event
+from registry_app.models import Item, Event, UserEvent
 
 
 admin.site.register(Item)
 admin.site.register(Event)
+admin.site.register(UserEvent)
